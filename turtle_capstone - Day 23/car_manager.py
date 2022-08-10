bfafceae3c64507b13bb578673d021bb37e492df
ryokushen-desk <98662283+Ryokushen@users.noreply.github.com>
2022-08-10 00:31:14 -0400

    Turtle Capstone - Day 23

--- turtle_capstone - Day 23/car_manager.py
+++ turtle_capstone - Day 23/car_manager.py
@@ -1,7 +1,25 @@
 from turtle import Turtle
+from random import choice, random, randrange
 
 COLORS = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
-class Car(Turtle)
+
+class Car(Turtle):
+    def __init__(self):
+        super().__init__(shape='square')
+        self.shapesize(stretch_wid=1, stretch_len=2)
+        self.penup()
+        self.setheading(-180)
+        self.color(choice(COLORS))
+        self.goto(300, (randrange(-230, 230)))
+
+
+    def car_drive(self):
+        self.fd(MOVE_INCREMENT)
+
+    def car_reset(self):
+        self.goto(300, (randrange(-230, 230)))
+
+
--- turtle_capstone - Day 23/main.py
+++ turtle_capstone - Day 23/main.py
@@ -1,23 +1,40 @@
 import time
-
 from turtle import Screen
 from player import Player, FINISH_LINE_Y
 from scoreboard import Scoreboard
+from car_manager import Car
 
 screen = Screen()
-screen.screensize(canvwidth=600, canvheight=600)
+screen.setup(width=600, height=600)
 screen.tracer(0)
 
 
 player = Player()
 scoreboard = Scoreboard()
 
+
 screen.listen()
 
+lst = []
+drivers_list = []
 game_is_on = True
+
+
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+# Move the cars and reset them after they go off-screen
+    for i in range(0, len(drivers_list)):
+        drivers_list[i].car_drive()
+        if drivers_list[i].xcor() < -320:
+            drivers_list[i].car_reset()
+
+# Generate new car every 6 game loops (kinda clunky but, it works)
+    lst.append(screen.update)
+    update_counter = len(lst)
+    if (update_counter % 6) == 0 and len(drivers_list) < 30:
+        drivers_list.append(Car())
 
 # Player movement across level
     screen.onkeypress(player.movement, 'Up')
@@ -27,7 +44,10 @@ while game_is_on:
         player.starting_position()
         scoreboard.update_score()
 
-
+# Detect collisions
+    for i in range(0, len(drivers_list)):
+        if player.distance(drivers_list[i]) < 15:
+            game_is_on = False
 
 
 
