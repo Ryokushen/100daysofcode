4b63065f04e364476c7debcd1a83ac528a67eee8
Charles Dorfeuille <ryokushen37@gmail.com>
2022-08-12 06:02:45 -0400

    File Manipulation - Day 24

--- files_day-24/scoreboard.py
+++ files_day-24/scoreboard.py
@@ -8,15 +8,23 @@ class Scoreboard(Turtle):
         self.ht()
         self.penup()
         self.speed('fastest')
-        self.counter = 0
+        self.score = 0
+        self.highscore = 0
         self.goto(50, 275)
-        self.write(f"Score: {self.counter} ", False, align='right', font=('Arial', 16, 'bold'))
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} ", False,
+                   align='right', font=('Arial', 16, 'bold'))
 
     def increase_score(self):
         self.clear()
-        self.counter += 1
-        self.write(f"Score: {self.counter} ", False, align='right', font=('Arial', 14, 'bold'))
+        self.score += 1
+        self.write(f"Score: {self.score} ", False,
+                   align='right', font=('Arial', 14, 'bold'))
 
     def game_over(self):
         self.goto((150, 0))
-        self.write(f"GAME OVER. Tsk, Tsk, Tsk. ", False, align='right', font=('Arial', 18, 'bold'))
+        self.write(f"GAME OVER. Tsk, Tsk, Tsk. ", False,
+                   align='right', font=('Arial', 18, 'bold'))
