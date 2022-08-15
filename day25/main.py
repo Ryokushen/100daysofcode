0e96ab2f2c8e2d9810009f5f4f1d907ec4ac4880
ryokushen-desk <98662283+Ryokushen@users.noreply.github.com>
2022-08-15 17:37:50 -0400

    Pandas and CSV - Day 25

--- day25/main.py
+++ day25/main.py
@@ -1,16 +1,18 @@
 
-# list_data = []
-# with open('weather_data.csv', mode='r') as template1:
-#     for data in template1.readlines():
-#         list_data.append(data.strip())
-#
-# print(list_data)
+import pandas
 
-import csv
-with open('weather_data.csv') as data_file:
-    data = csv.reader(data_file)
-    temperatures = []
-    for row in data:
-        temperatures.append(row[1])
-    temperatures[1:] = [int(x) for x in temperatures[1:]]
-    print(temperatures[1:])
+data = pandas.read_csv('2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv')
+fur_color = data['Primary Fur Color'].drop_duplicates()
+colors = fur_color.tolist()
+
+fur_color_gray_count = sum(data['Primary Fur Color'] == 'Gray')
+fur_color_cin_count = sum(data['Primary Fur Color'] == 'Cinnamon')
+fur_color_black_count = sum(data['Primary Fur Color'] == 'Black')
+counts = [fur_color_gray_count, fur_color_cin_count, fur_color_black_count]
+
+data1 = {'color': colors[1:],
+         'count': counts
+         }
+data2 = pandas.DataFrame(data1)
+data2.to_csv('squirrel_count.csv')
+
