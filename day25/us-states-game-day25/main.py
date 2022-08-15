7cc6bcb7478e9983867fd30e6efc0655b25d28c5
ryokushen-desk <98662283+Ryokushen@users.noreply.github.com>
2022-08-15 19:23:38 -0400

    US States Game - Day 25

--- day25/us-states-game-day25/main.py
+++ day25/us-states-game-day25/main.py
@@ -9,20 +9,23 @@ screen.addshape(image)
 screen.setup(width=750, height=525)
 turtle.shape(image)
 
+# Turn CSV Series into list
 data = pandas.read_csv('50_states.csv')
 data_states = data.state
 list_of_states = data_states.to_list()
 
 correct = []
-game_is_on = True
 
-while game_is_on:
 
+while len(correct) < 50:
     answer_state = screen.textinput(title=f'{len(correct)}/50 Correct', prompt="What's another state's name?").title()
     for state in list_of_states:
+        # Compare input to list of states in CSV
         if state == answer_state:
+            # Write state name on coords on map
             state_xcoord = int(data[data_states == state].x)
             state_ycoord = int(data[data_states == state].y)
+            # Add correct guess to list to keep track of score
             correct.append(state)
             writing = State()
             writing.goto(x=state_xcoord, y=state_ycoord)
@@ -31,3 +34,4 @@ while game_is_on:
             continue
 
 
+screen.exitonclick()
