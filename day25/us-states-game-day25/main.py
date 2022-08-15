90670ce8ecfcbd4335af273fcc4b06f101809a82
ryokushen-desk <98662283+Ryokushen@users.noreply.github.com>
2022-08-15 19:14:57 -0400

    US States Game - Day 25

--- day25/us-states-game-day25/main.py
+++ day25/us-states-game-day25/main.py
@@ -2,33 +2,32 @@ import turtle
 import pandas
 from mapped_states import State
 
-# screen = turtle.Screen()
-# screen.title("U.S. States Game")
-# image = 'blank_states_img.gif'
-# screen.addshape(image)
-# turtle.shape(image)
-
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = 'blank_states_img.gif'
+screen.addshape(image)
+screen.setup(width=750, height=525)
+turtle.shape(image)
 
 data = pandas.read_csv('50_states.csv')
 data_states = data.state
 list_of_states = data_states.to_list()
 
-print(state_xcoord)
 correct = []
 game_is_on = True
 
 while game_is_on:
 
-    answer_state = screen.textinput(title="Guess the State!", prompt="What's another state's name?").title()
+    answer_state = screen.textinput(title=f'{len(correct)}/50 Correct', prompt="What's another state's name?").title()
     for state in list_of_states:
         if state == answer_state:
-            state_xcoord = data[data_states == state].x
-            state_ycoord = data[data_states == state].y
-
+            state_xcoord = int(data[data_states == state].x)
+            state_ycoord = int(data[data_states == state].y)
             correct.append(state)
             writing = State()
-            writing.goto()
-            print(correct)
-
+            writing.goto(x=state_xcoord, y=state_ycoord)
+            writing.write(f'{state}')
+        else:
+            continue
 
 
