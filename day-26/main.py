945048666607413499e6d384753dcdc2cfb9f6c2
ryokushen-desk <98662283+Ryokushen@users.noreply.github.com>
2022-08-17 17:31:03 -0400

    List, Dictionary Comprehension and Iterating through Data

--- day-26/main.py
+++ day-26/main.py
@@ -31,6 +31,6 @@ nato_dict = {row.letter:row.code for (index, row) in nato_data.iterrows()}
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
 word = input("Enter a word: \n").upper()
-list = [nato_dict.get(letter) for letter in word]
+list = [nato_dict[letter] for letter in word]
 print(list)
 
