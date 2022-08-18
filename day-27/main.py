03b8cfeb710d8901045d3bf62e865d82cbab43b8
ryokushen-desk <98662283+Ryokushen@users.noreply.github.com>
2022-08-18 16:22:37 -0400

    Tkinter and GUI's - Day 27

---
+++ day-27/main.py
@@ -0,0 +1,22 @@
+import tkinter
+
+# Create window object, change title and manipulate size
+window = tkinter.Tk()
+window.title('My First GUI Program')
+window.minsize(width=500, height=300)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
