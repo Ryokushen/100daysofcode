b5ab02aa47519569f4809b211833479623eaa5af
ryokushen-desk <98662283+Ryokushen@users.noreply.github.com>
2022-08-21 23:38:08 -0400

    Pomodoro Tomato - Day 28

--- pomodoro_day28/main.py
+++ pomodoro_day28/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -8,20 +9,61 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
+reps = 0
+timer = None
+mark = ""
 # ---------------------------- TIMER RESET ------------------------------- # 
+
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text='00:00')
+    timer_label.config(text='Timer', font=(FONT_NAME, 40, 'bold'), fg=GREEN, bg=YELLOW)
+    checkmark.config(text="" ,fg=GREEN, bg=YELLOW, font=(FONT_NAME, 16, 'bold'))
+    global reps
+    reps = 0
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(5)
+    global reps
+    reps += 1
+    if reps % 8 == 0:
+        break_sec = LONG_BREAK_MIN * 60
+        count_down(break_sec)
+        timer_label.config(fg=RED, text='Break')
+    elif reps % 2 == 0:
+        count_down(SHORT_BREAK_MIN * 60)
+        timer_label.config(fg=PINK, text='Break')
+    else:
+        work_sec = WORK_MIN * 60
+        count_down(work_sec)
+        timer_label.config(fg=GREEN, text='Work')
+
+
+
 
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
-    canvas.itemconfig(timer_text, text=count)
+
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f'{count_min}:{count_sec}')
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        mark = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            mark += "✔"
+        checkmark.config(text=mark)
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title('Pomodoro Study Counter')
@@ -30,13 +72,13 @@ window.config(padx=100, pady=50, bg=YELLOW)
 
 timer_label = Label(text='Timer', font=(FONT_NAME, 40, 'bold'), fg=GREEN, bg=YELLOW)
 timer_label.grid(column=1, row=0)
-checkmark = Label(text="✓", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 16, 'bold'))
+checkmark = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 16, 'bold'))
 checkmark.grid(column=1, row=3)
 
 start_button = Button(text='Start', command=start_timer)
 start_button.grid(column=0, row=2)
 start_button.grid(column=0, row=2)
-reset_button = Button(text='Reset')
+reset_button = Button(text='Reset', command=reset_timer)
 reset_button.grid(column=2, row=2)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
