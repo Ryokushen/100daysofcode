4918ff6627b3b67a33c09f2b656e28fff7050d70
ryokushen-desk <98662283+Ryokushen@users.noreply.github.com>
2022-09-02 13:39:56 -0400

    Email with Python - Day 32

--- SMTP-Day32/main.py
+++ SMTP-Day32/main.py
@@ -1,10 +1,30 @@
 import smtplib
+from random import randint
+import datetime as dt
 
 my_email = "ryokushen37.emailtesting@gmail.com"
 my_password = 'dnusjddnuzmhsmym'
 
-connection = smtplib.SMTP("smtp.gmail.com", port=587)
-connection.starttls()
-connection.login(user=my_email, password=my_password)
-connection.sendmail(from_addr=my_email, to_addrs="ryokushen37@yahoo.com", msg="Hello World.")
-connection.close()
+
+def send_email(quote):
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=my_password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs="ryokushen37@yahoo.com",
+            msg=f"Subject:Mondays' Quote\n\n{quote}."
+        )
+
+now = dt.datetime.now()
+day_of_week = now.weekday()
+
+with open("quotes.txt", "r") as quotes:
+    quotelist = quotes.readlines()
+    if day_of_week == 4:
+        send_email(quotelist[randint(0, 102)])
+
+
+
+
+
