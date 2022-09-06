587703d850722c10f9dc08c031f6d964a83a035e
ryokushen-desk <98662283+Ryokushen@users.noreply.github.com>
2022-09-06 17:15:36 -0400

    Updated Day 33

--- API_day33/issoverhead-start/main.py
+++ API_day33/issoverhead-start/main.py
@@ -6,7 +6,7 @@ from smtplib import SMTP
 MY_LAT = 36.755169 # Your latitude
 MY_LONG = -76.237442 # Your longitude
 MY_EMAIL = "ryokushen37.emailtesting@gmail.com"
-MY_PASSWORD = "6KHEfXgzsYvg7Bv"
+MY_PASSWORD = "llywlvusrnovdgok"
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
@@ -14,10 +14,6 @@ data = response.json()
 
 iss_latitude = float(data["iss_position"]["latitude"])
 iss_longitude = float(data["iss_position"]["longitude"])
-
-#Your position is within +5 or -5 degrees of the ISS position.
-# print(iss_longitude)
-# print(iss_latitude)
 
 def compare_pos():
     if MY_LAT - 5 < iss_latitude < MY_LAT + 5 and MY_LONG - 5 < iss_longitude < MY_LONG + 5:
@@ -39,18 +35,14 @@ sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
 time_now = datetime.now()
 current_hour = time_now.hour
-#If the ISS is close to my current position
-# and it is currently dark
-# Then send me an email to tell me to look up.
 # BONUS: run the code every 60 seconds.
 
 if compare_pos() == True and sunset < current_hour < sunrise:
     with SMTP("smtp.gmail.com", port=587) as template:
-        template.connect
-        template.starttls
-        template.login(email=MY_EMAIL, password=MY_PASSWORD)
-        template.send_message(from_addr=MY_EMAIL, to_addrs="ryokushen37@yahoo.com"
-        msg="Subject:\n\n The ISS is above you. Look Outside! :)")
+        template.starttls()
+        template.login(user=MY_EMAIL, password=MY_PASSWORD)
+        template.sendmail(from_addr=MY_EMAIL, to_addrs='ryokushen37@yahoo.com',
+        msg="Subject:International Space Station\n\n The ISS is above you. Look Outside! :)")
 
 
 
