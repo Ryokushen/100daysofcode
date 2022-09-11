b0eaa4b2077ff3d7e63b2eb06b209e9f0a2012de
ryokushen-desk <98662283+Ryokushen@users.noreply.github.com>
2022-09-11 08:47:55 -0400

    Final Day 34

--- Quiz_App-Day34/quizzler-app-start/ui.py
+++ Quiz_App-Day34/quizzler-app-start/ui.py
@@ -30,13 +30,40 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz.")
+            self.correct_btn.config(state="disabled")
+            self.wrong_btn.config(state="disabled")
 
     def trueanswer(self):
-        self.quiz.check_answer("True")
-        self.get_next_question()
+        is_right = self.quiz.check_answer("True")
+        self.give_feedback(is_right)
+
+        self.score_label.config(text=f"Score: {self.quiz.score}")
+
 
     def falseanswer(self):
-        self.quiz.check_answer("False")
-        self.get_next_question()
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+        self.score_label.config(text=f"Score: {self.quiz.score}")
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+
+        self.window.after(1000, self.get_next_question)
+
+
+
+
+
+
+
+
+
