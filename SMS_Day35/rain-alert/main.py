5effca19a1b040e24e162ec18fd665632d7f5615
Charles Dorfeuille <ryokushen37@gmail.com>
2022-09-12 19:27:26 -0400

    Streamlined 48 hour API Call

--- SMS_Day35/rain-alert/main.py
+++ SMS_Day35/rain-alert/main.py
@@ -1,1 +1,17 @@
-api_key = "69f04e4613056b159c2761a9d9e664d2"
+import requests
+
+API_KEY = "69f04e4613056b159c2761a9d9e664d2"
+
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+
+weather_params = {
+    "lat": 36.755169,
+    "lon": -76.237442,
+    "appid": API_KEY
+
+}
+r = requests.get(OWM_Endpoint, params=weather_params)
+r.raise_for_status()
+data = r.json()
+print(data['hourly'])
