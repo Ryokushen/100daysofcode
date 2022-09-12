280fcf30ff949cc892377e07056f972c155ba206
Charles Dorfeuille <ryokushen37@gmail.com>
2022-09-12 19:38:50 -0400

    Exclude Minutely, Current, Daily

--- SMS_Day35/rain-alert/main.py
+++ SMS_Day35/rain-alert/main.py
@@ -1,17 +1,16 @@
 import requests
 
 API_KEY = "69f04e4613056b159c2761a9d9e664d2"
-
-
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
 
 weather_params = {
     "lat": 36.755169,
     "lon": -76.237442,
-    "appid": API_KEY
+    "appid": API_KEY,
+    "exclude": "current,minutely,daily"
 
 }
 r = requests.get(OWM_Endpoint, params=weather_params)
 r.raise_for_status()
-data = r.json()
-print(data['hourly'])
+weather_data = r.json()
+print(weather_data)
