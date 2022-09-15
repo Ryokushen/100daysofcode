d46cb315a6ef99cb74c84d13729bd3dec71beaa7
Charles Dorfeuille <ryokushen37@gmail.com>
2022-09-15 13:22:31 -0400

    FInal Email Version

--- SMS_Day35/rain-alert/main.py
+++ SMS_Day35/rain-alert/main.py
@@ -1,15 +1,18 @@
+from email import message
 from re import X
+from sqlite3 import connect
 import requests
+import smtplib
 
 API_KEY = "69f04e4613056b159c2761a9d9e664d2"
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
-
-#  "lat": 36.755169,
-#     "lon": -76.237442,
+MY_EMAIL = 'ryokushen37.emailtesting@gmail.com'
+MY_PASSWORD = 'bsisfmbvpjecznrq'
+TEST_EMAIL = 'ryokushen37@yahoo.com'
 
 weather_params = {
-    "lat": 49.29,
-    "lon": -92.3,
+    "lat": 36.755169,
+    "lon": -76.237442,
     "appid": API_KEY,
     "exclude": "current,minutely,daily"
 
@@ -29,4 +32,16 @@ weather_status = ""
 for num in first_12_weather:
     if num < 700:
         weather_status = "Bring an umbrella!"
-print(weather_status)
+#----------------------------------------------------------EMAIL--------------------------------------------------------------------#
+
+
+def whatistheweather(weather):
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs=TEST_EMAIL,
+                            msg=f"Subject: Weather Status \n\n {weather}")
+
+
+whatistheweather(weather_status)
+print('Done...')
