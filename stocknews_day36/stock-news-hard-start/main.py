35050ea0fd4eeccaf1919c081adef75697f5ea6a
ryokushen-desk <98662283+Ryokushen@users.noreply.github.com>
2022-09-18 21:29:17 -0400

    Main Finished

--- stocknews_day36/stock-news-hard-start/main.py
+++ stocknews_day36/stock-news-hard-start/main.py
@@ -31,7 +31,7 @@ def comparison(num1, num2):
     except ZeroDivisionError:
         return 0
 
-def send_message(listofarts):
+def send_message(listofarts, num1, num2):
    
     account_sid = os.environ.get("TWILIO_SID")
     auth_token = os.environ.get("TWILIO_AUTH")
@@ -41,7 +41,7 @@ def send_message(listofarts):
 
         message = client.messages \
                 .create(
-                     body=f"Tesla: {round(comparison(comp3, comp4),2)}\n\nHeadline: {article['title']}\n\nBrief: {article['description']}",
+                     body=f"Tesla: {round(comparison(num1, num2),2)}\n\nHeadline: {article['title']}\n\nBrief: {article['description']}",
                      from_='+18147476197',
                      to='+13218908318'
                  )
@@ -62,7 +62,7 @@ if dayofweek == 6:
         new_arts = g.json()
         top_articles = new_arts['articles'][:3]
         listofarts = [article for article in top_articles]
-        send_message(listofarts)
+        send_message(listofarts, comp3, comp4)
        
 
 #Monday-Saturday
@@ -71,48 +71,14 @@ else:
     twodaysago = today - timedelta(days=2)
     comp1 = float(daily[str(yest)]['4. close'])
     comp2 = float(daily[str(twodaysago)]['4. close'])
-   
+    if comparison(comp1, comp2) < 5:
+        g = requests.get(NEWS_ENDPOINT)
+        g.raise_for_status
+        new_arts = g.json()
+        top_articles = new_arts['articles'][:3]
+        listofarts = [article for article in top_articles]
+        send_message(listofarts, comp1, comp2)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-## STEP 1: Use https://newsapi.org/docs/endpoints/everything
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-#HINT 1: Get the closing price for yesterday and the day before yesterday. Find the positive difference between the two prices. e.g. 40 - 20 = -20, but the positive difference is 20.
-#HINT 2: Work out the value of 5% of yerstday's closing stock price. 
-
-
-
-## STEP 2: Use https://newsapi.org/docs/endpoints/everything
-# Instead of printing ("Get News"), actually fetch the first 3 articles for the COMPANY_NAME. 
-#HINT 1: Think about using the Python Slice Operator
-
-
-
-## STEP 3: Use twilio.com/docs/sms/quickstart/python
-# Send a separate message with each article's title and description to your phone number. 
-#HINT 1: Consider using a List Comprehension.
 
 
 
