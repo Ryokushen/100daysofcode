e2914e024ff87e435ae77783e5635435c6b62d3a
Charles Dorfeuille <ryokushen37@gmail.com>
2022-09-30 09:44:51 -0400

    update data manager

--- flightcapstone_day39/data_manager.py
+++ flightcapstone_day39/data_manager.py
@@ -1,3 +1,14 @@
+from pprint import pprint
+import requests
+
+SHEETY_ENDPOINT = "https://api.sheety.co/fbba0bd391a802039df3378c3f03280e/day39CapstoneFlightDeals/prices"
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+
+    # This class is responsible for talking to the Google Sheet.
+    def __init__(self):
+        self.request = requests.get(url=SHEETY_ENDPOINT)
+        self.request.raise_for_status
+        r = self.request.json()
+        pprint(r)
--- flightcapstone_day39/main.py
+++ flightcapstone_day39/main.py
@@ -1,10 +1,5 @@
 # This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from data_manager import DataManager
 
-from pprint import pprint
-import requests
-SHEETY_ENDPOINT = "https://api.sheety.co/fbba0bd391a802039df3378c3f03280e/day39CapstoneFlightDeals/prices"
-
-request = requests.get(url=SHEETY_ENDPOINT)
-request.raise_for_status
-r = request.json()
-pprint(r)
+sheet_data = DataManager()
+print(sheet_data)
