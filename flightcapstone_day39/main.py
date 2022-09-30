8dbdc3916a2fcbdc7dfb9eb5bab0a6e34131f13f
Charles Dorfeuille <ryokushen37@gmail.com>
2022-09-30 09:36:03 -0400

    Working

--- flightcapstone_day39/main.py
+++ flightcapstone_day39/main.py
@@ -1,1 +1,10 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+# This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+from pprint import pprint
+import requests
+SHEETY_ENDPOINT = "https://api.sheety.co/fbba0bd391a802039df3378c3f03280e/day39CapstoneFlightDeals/prices"
+
+request = requests.get(url=SHEETY_ENDPOINT)
+request.raise_for_status
+r = request.json()
+pprint(r)
