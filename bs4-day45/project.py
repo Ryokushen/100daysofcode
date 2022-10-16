2d0206a6c2bd120d84cf8ab68fccc43e43f82086
ryokushen <ryokushen37@gmail.com>
2022-10-16 10:44:53 -0400

    Print Most upvoted article

--- bs4-day45/project.py
+++ bs4-day45/project.py
@@ -6,20 +6,23 @@ yc_webpage = response.text
 
 soup = BeautifulSoup(yc_webpage, "html.parser")
 
-article_tag = soup.find(name="span", class_="titleline")
-article_text = article_tag.getText()
-print(article_text)
+article_texts = []
+article_links = []
 
-article_href = soup.find(name="span", class_="titleline")
-article_link = article_href.contents[0].get("href")
-print(article_link)
+articles = soup.find_all(name="span", class_="titleline")
+for article_tag in articles:
+    text = article_tag.getText()
+    article_texts.append(text)
+    link = article_tag.contents[0].get("href")
+    article_links.append(link)
 
-article_score = soup.find(name="span", class_="score")
-article_upvote = article_score.getText()
-print(article_upvote)
+article_upvotes = [int(score.getText().split()[0]) for score in soup.find_all(name="span", class_="score")]
 
+# print(article_texts)
+# print(article_links)
+# print(article_upvotes)
 
-# all_anchor_tags = soup.find_all(name="a")
+largest_number = max(article_upvotes)
+index_num = article_upvotes.index(largest_number)
+print(article_texts[index_num], article_links[index_num], article_upvotes[index_num])
 
-# for tag in all_anchor_tags:
-#     print(tag.getText())
