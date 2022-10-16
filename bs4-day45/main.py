7cfbce89462569600c8fe2241392b3d7f9a423d5
ryokushen <ryokushen37@gmail.com>
2022-10-16 06:35:51 -0400

    Starting work on BS4

--- bs4-day45/main.py
+++ bs4-day45/main.py
@@ -1,5 +1,27 @@
 from bs4 import BeautifulSoup
+# import lxml
 
-with open(file="website.html", mode="r", encoding='utf-8') as website:
+# Open website as a readable object
+with open(file="website.html", mode="r") as website:
     contents = website.read()
-    print(contents)
+
+# Import bs4 module and pass readable object and parser
+soup = BeautifulSoup(contents, "html.parser")
+# print(soup.title)
+# print(soup.title.name)
+
+# Can use prettify to view HTML structure
+# print(soup.prettify())
+
+# Use find_all fxn to find all occurrences of a tag
+# all_anchor_tags = soup.find_all(name="a")
+# print(all_anchor_tags)
+
+# for tag in all_anchor_tags:
+#     # print(tag.get("href"))
+#     # print(tag.getText())
+#     pass
+
+# Find individual heading with class
+heading = soup.find(name="h1", id="name")
+print(heading)
