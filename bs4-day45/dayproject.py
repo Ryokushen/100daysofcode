a36c3dad0c37ffc357ce2fab110c269fc58415d8
ryokushen <ryokushen37@gmail.com>
2022-10-17 11:21:00 -0400

    Start Final days project continued

--- bs4-day45/dayproject.py
+++ bs4-day45/dayproject.py
@@ -8,6 +8,15 @@ movie_website = response.text
 
 soup = BeautifulSoup(movie_website, "html.parser")
 
+movie_list = []
 movie_titles = soup.find_all(name="h3", class_="title")
 for movies in movie_titles:
-    print(movies.getText())
+    movienum = movies.getText()
+    movie_list.append(movienum)
+
+movie_list.reverse()
+print(movie_list)
+for movie in movie_list:
+    with open("movie-list.txt", "a") as file:
+        file.write(movie)
+        file.write("\n")
